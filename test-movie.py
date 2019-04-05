fce1773e207d9ba1c7d4a32b4d5b8e533de9714f
Ethan <ethan.r.davidson@gmail.com>
2019-04-05 01:13:59 -0700

    im just tryna get my stuff to work :(

--- test-movie.py
+++ test-movie.py
@@ -1,4 +1,5 @@
 # py test-movie.py
+import subprocess
 from moviepy.editor import *
 
 # Main Process
@@ -10,10 +11,36 @@ clip2AudioPath = "C:\\Users\\acer\\Documents\\GitHub\\reddit-movie-maker\\db\\20
 
 audClip1 = AudioFileClip(clip1AudioPath)
 audClip2 = AudioFileClip(clip2AudioPath)
+dur = audClip1.duration + audClip2.duration
 clips = [
     ImageClip(clip1PhotoPath).set_fps(fps).set_duration(audClip1.duration).set_audio(audClip1),
     ImageClip(clip2PhotoPath).set_fps(fps).set_duration(audClip2.duration).set_audio(audClip2)
 ]
 
-final_video = concatenate_videoclips(clips, method='chain')
-final_video.write_videofile("test.mp4", fps=fps)
+ffmpeg = "C:\\ffmpeg\\bin\\ffmpeg.exe"
+subprocess.call([
+    ffmpeg,
+    "-f",
+    "concat",
+    "-safe",
+    "0",
+    "-i",
+    "photos.txt",
+    # "-vsync",
+    # "vfr",
+    # "-pix_fmt",
+    # "yuv420p",
+    "C:\\Users\\acer\\Documents\\GitHub\\reddit-movie-maker\\test1.mp4"
+])
+
+# write concat demuxer photos.txt for all images using audio durations
+# ffmpeg -f concat -i photos.txt test1.mp4
+# ffmpeg -f concat -i photos.txt -vsync vfr -pix_fmt yuv420p output.mp4
+# write concat demuxer audio.txt for all audio files
+# ffmpeg -f concat -i audio.txt test1.mp3
+# ffmpeg -f concat -safe 0 -i audio.txt -c copy output.mp3
+# combine the audio file and the video file
+# ffmpeg -i test1.mp4 -i test1.mp3 -codec copy -shortest output.mp3
+
+# final_video = concatenate_videoclips(clips, method='chain')
+# final_video.write_videofile("test.mp4", fps=fps)
