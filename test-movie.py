0c04cc230637a212ff69fe344f6cf8eff3722f34
Ethan <ethan.r.davidson@gmail.com>
2019-06-06 22:40:48 -0700

    coo it works?

--- test-movie.py
+++ test-movie.py
@@ -2,20 +2,23 @@
 from moviepy.editor import *
 
 # Main Process
-clip1PhotoPath = "C:\\Users\\acer\\Documents\\GitHub\\reddit-movie-maker\\db\\2019\\04\\01\\1554155961\\photos\\0\\0.jpeg"
-clip1AudioPath = "C:\\Users\\acer\\Documents\\GitHub\\reddit-movie-maker\\db\\2019\\04\\01\\1554155961\\audio\\0\\0.mp3"
-clip2PhotoPath = "C:\\Users\\acer\\Documents\\GitHub\\reddit-movie-maker\\db\\2019\\04\\01\\1554155961\\photos\\0\\1.jpeg"
-clip2AudioPath = "C:\\Users\\acer\\Documents\\GitHub\\reddit-movie-maker\\db\\2019\\04\\01\\1554155961\\audio\\0\\1.mp3"
+clip1PhotoPath = "C:/Users/acer/Documents/GitHub/reddit-movie-maker/test/0.png"
+clip1AudioPath = "C:/Users/acer/Documents/GitHub/reddit-movie-maker/test/0.mp3"
 
 audClip1 = AudioFileClip(clip1AudioPath)
-audClip2 = AudioFileClip(clip2AudioPath)
-clips = [
-    ImageClip(clip1PhotoPath).set_duration(audClip1.duration).set_audio(audClip1),
-    ImageClip(clip2PhotoPath).set_duration(audClip2.duration).set_audio(audClip2)
-]
+clip = ImageClip(clip1PhotoPath).set_duration(audClip1.duration).set_audio(audClip1)
 
-final_video = concatenate_videoclips(clips)
-final_video.write_videofile("test.mp4")
+# audClip1 = AudioFileClip(clip1AudioPath)
+# audClip2 = AudioFileClip(clip2AudioPath)
+# clips = [
+#     ImageClip(clip1PhotoPath).set_duration(audClip1.duration).set_audio(audClip1),
+#     ImageClip(clip2PhotoPath).set_duration(audClip2.duration).set_audio(audClip2)
+# ]
+#
+# final_video = concatenate_videoclips(clips)
+# final_video.write_videofile("test.mp4")
+
+clip.write_videofile("./test/test.mp4", fps=24)
 
 
 
