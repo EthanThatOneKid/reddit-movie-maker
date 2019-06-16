a9083936ffdc1ba4a04dc69e83f343f074190ebb
Ethan <ethan.r.davidson@gmail.com>
2019-06-16 16:39:47 -0700

    Update batch.py

--- batch.py
+++ batch.py
@@ -2,6 +2,9 @@
 import sys, subprocess
 
 # Main Process
+
+## Reading Batch Config
+print("Reading Batch Config")
 try:
     batch_config_path = sys.argv[1]
 except:
@@ -12,5 +15,13 @@ for post in batch_document.split("\n"):
     command = "py main.py {}".format(post)
     commands.append(command)
 command = " & ".join(commands)
+
+## Executing Batch Command
+print("Executing Batch Command")
+print("Creating {} videos".format(len(commands)))
+print("Command: {}".format(command))
 subprocess.call(command)
+
+## All Done
+print("All Done")
 exit()
